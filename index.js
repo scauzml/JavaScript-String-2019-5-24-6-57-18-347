fa25616982ea4b7a286f25f54338da91293d4033
scauzml <13828607279@163.com>
2019-06-10 17:37:36 +0800

    张明亮提交

--- index.js
+++ index.js
@@ -4,13 +4,20 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
+console.log(name.toLocaleUpperCase());
 
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+var result=sentence.replace("g","G");
+var result=result.replace("a","A");
+var result=result.replace("m","M");
+var result=result.replace("m","M");
+console.log(result);
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
+console.log(money.substr(1));
+
 //TODO write your code here...
